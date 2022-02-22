a36387188d9a131f08dc57e0ba8a98ef7dda1cdb
Cade Jorgensen <cade25k@byu.edu>
2022-02-21 17:00:56 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission7.Models;
+using Mission7.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        // Constructor
+        public HomeController(IBookstoreRepository br)
+        {
+            repo = br;
+        }
+
+        // Get Index page
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+            var bvm = new BooksViewModel
+            {
+                Books = repo.Books
+                    .OrderBy(b => b.Title)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(bvm);
+        }
+    }
+}
